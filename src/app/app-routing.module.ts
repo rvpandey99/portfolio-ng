f15e4d6588aa79d1d7523604f413da29434215d3
Ravi Prakash Pandey <rvpandey99@gmail.com>
2019-10-13 14:55:13 +0530

    added photo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { HireComponent } from './hire/hire.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'home', component: HomeComponent},
+  {path:'blog', component: BlogComponent},
+  {path:'hire', component: HireComponent},
+  {path:'**', component: HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
