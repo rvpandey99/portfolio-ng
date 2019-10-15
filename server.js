1ae19da7915f9e2ead641cc60bdd262a8fce0afe
Ravi Prakash Pandey <rvpandey99@gmail.com>
2019-10-15 22:56:57 +0000

    added dotenv

---
+++ server.js
@@ -0,0 +1,24 @@
+"use strict";
+const express = require("express");
+require('dotenv').config();
+const compression = require("compression");
+
+const _port = 4100;
+const _app_folder = 'dist/raviportfolio';
+
+const app = express();
+app.use(compression());
+
+
+// ---- SERVE STATIC FILES ---- //
+app.server.get('*.*', express.static(_app_folder, {maxAge: '1y'}));
+
+// ---- SERVE APLICATION PATHS ---- //
+app.all('*', function (req, res) {
+    res.status(200).sendFile(`/`, {root: _app_folder});
+});
+
+// ---- START UP THE NODE SERVER  ----
+app.listen(_port, function () {
+    console.log("Node Express server for " + app.name + " listening on http://localhost:" + _port);
+});
